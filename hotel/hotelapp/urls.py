e054667735b13b9c70cc1f99b4d31585ff610b2f
A00447454 <sunaritha.rajendra.karanth@smu.ca>
2022-03-13 21:52:27 -0300

    Final Commit

---
+++ hotel/hotelapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path("getListOfHotels/", views.Hotel_List, name="hotels"),
+    path('reservationConfirmation/', views.ReservationList, name="home"),
+
+]
